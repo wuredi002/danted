9fe2452eea5b8187f4a4bb297c8ee9f0ccac7245
wuredi002 <466114995@qq.com>
2025-03-08 18:21:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-#   Dante Socks5 Server AutoInstall
-#   -- Owner:       https://www.inet.no/dante
-#   -- Provider:    https://sockd.info
-#   -- Author:      Lozy
+#   Dante Socks5 服务器自动安装
+#   -- 所有者:       https://www.inet.no/dante
+#   -- 提供者:      https://sockd.info
+#   -- 作者:        Lozy
 #
 
-# Check if user is root
+# 检查是否是 root 用户
 if [ $(id -u) != "0" ]; then
-    echo "Error: You must be root to run this script, please use root to install"
+    echo "错误: 必须使用 root 用户运行此脚本，请使用 root 用户安装"
     exit 1
 fi
 
@@ -18,39 +18,41 @@ SYSTEM_RECOGNIZE=""
 
 [ "$1" == "--no-github" ] && REQUEST_SERVER=${SCRIPT_SERVER}
 
+# 判断操作系统类型
 if [ -s "/etc/os-release" ];then
     os_name=$(sed -n 's/PRETTY_NAME="\(.*\)"/\1/p' /etc/os-release)
 
     if [ -n "$(echo ${os_name} | grep -Ei 'Debian|Ubuntu' )" ];then
-        printf "Current OS: %s\n" "${os_name}"
+        printf "当前操作系统: %s\n" "${os_name}"
         SYSTEM_RECOGNIZE="debian"
 
     elif [ -n "$(echo ${os_name} | grep -Ei 'CentOS')" ];then
-        printf "Current OS: %s\n" "${os_name}"
+        printf "当前操作系统: %s\n" "${os_name}"
         SYSTEM_RECOGNIZE="centos"
     else
-        printf "Current OS: %s is not support.\n" "${os_name}"
+        printf "当前操作系统: %s 不支持.\n" "${os_name}"
     fi
 elif [ -s "/etc/issue" ];then
     if [ -n "$(grep -Ei 'CentOS' /etc/issue)" ];then
-        printf "Current OS: %s\n" "$(grep -Ei 'CentOS' /etc/issue)"
+        printf "当前操作系统: %s\n" "$(grep -Ei 'CentOS' /etc/issue)"
         SYSTEM_RECOGNIZE="centos"
     else
         printf "+++++++++++++++++++++++\n"
         cat /etc/issue
         printf "+++++++++++++++++++++++\n"
-        printf "[Error] Current OS: is not available to support.\n"
+        printf "[错误] 当前操作系统: 不支持.\n"
     fi
 else
-    printf "[Error] (/etc/os-release) OR (/etc/issue) not exist!\n"
-    printf "[Error] Current OS: is not available to support.\n"
+    printf "[错误] (/etc/os-release) 或 (/etc/issue) 文件不存在！\n"
+    printf "[错误] 当前操作系统: 不支持.\n"
 fi
 
+# 如果系统支持，下载并执行安装脚本
 if [ -n "$SYSTEM_RECOGNIZE" ];then
     wget -qO- --no-check-certificate ${REQUEST_SERVER}/install_${SYSTEM_RECOGNIZE}.sh | \
         bash -s -- $*  | tee /tmp/danted_install.log
 else
-    printf "[Error] Installing terminated"
+    printf "[错误] 安装终止\n"
     exit 1
 fi
 
