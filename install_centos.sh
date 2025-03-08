ff2a6124226650c82e10d9afd57c97e2adddc8a0
wuredi002 <466114995@qq.com>
2025-03-08 18:06:25 +0800

    Update install_centos.sh

--- install_centos.sh
+++ install_centos.sh
@@ -1,55 +1,59 @@
 #!/bin/bash
 #
-# Provides:          sockd.info (Lozy)
+# 提供服务: sockd.info (Lozy)
 #
 
-VERSION="1.3.2"
-INSTALL_FROM="compile"
-DEFAULT_PORT="2016"
-DEFAULT_USER=""
-DEFAULT_PAWD=""
-WHITE_LIST_NET=""
-WHITE_LIST=""
-SCRIPT_HOST="https://public.sockd.info"
-PACKAGE_NAME="dante_1.3.2-1_$(uname -m).deb"
-COLOR_PATH="/etc/default/color"
+VERSION="1.4.4"  # 版本号
+INSTALL_FROM="compile"  # 安装方式（编译）
+DEFAULT_PORT="2024"  # 默认端口
+DEFAULT_USER=""  # 默认用户名
+DEFAULT_PAWD=""  # 默认密码
+WHITE_LIST_NET=""  # IP 白名单网络
+WHITE_LIST=""  # 白名单
+SCRIPT_HOST="https://public.sockd.info"  # 脚本服务器地址
+PACKAGE_NAME="dante_1.3.2-1_$(uname -m).deb"  # 安装包名称
+COLOR_PATH="/etc/default/color"  # 颜色配置路径
 
-BIN_DIR="/etc/danted"
-BIN_PATH="/etc/danted/sbin/sockd"
-CONFIG_PATH="/etc/danted/sockd.conf"
-BIN_SCRIPT="/etc/init.d/sockd"
+BIN_DIR="/etc/danted"  # 二进制文件存放目录
+BIN_PATH="/etc/danted/sbin/sockd"  # 二进制可执行文件路径
+CONFIG_PATH="/etc/danted/sockd.conf"  # 配置文件路径
+BIN_SCRIPT="/etc/init.d/sockd"  # 启动脚本路径
 
-DEFAULT_IPADDR=$(ip addr | grep 'inet ' | grep -Ev 'inet 127|inet 192\.168' | \
-            sed "s/[[:space:]]*inet \([0-9.]*\)\/.*/\1/")
-RUN_PATH=$(cd `dirname $0`;pwd )
-RUN_OPTS=$*
+# 获取服务器的外部 IP 地址（排除本地回环地址和私有地址）
+DEFAULT_IPADDR=$(ip addr | grep 'inet ' | grep -Ev 'inet 127|inet 192\.168' | sed "s/[[:space:]]*inet \([0-9.]*\)\/.*/\1/")
+RUN_PATH=$(cd `dirname $0`;pwd )  # 运行脚本所在路径
+RUN_OPTS=$*  # 运行时传入的参数
 
-##################------------Func()---------#####################################
+##################------------函数定义---------#####################################
+# 移除已安装的 Dante 服务
 remove_install(){
     [ -s "${BIN_SCRIPT}" ] && ${BIN_SCRIPT} stop > /dev/null 2>&1
     [ -f "${BIN_SCRIPT}" ] && rm "${BIN_SCRIPT}"
     [ -n "$BIN_DIR" ] && rm -r "$BIN_DIR"
 }
 
+# 检测是否已安装 Dante
 detect_install(){
     if [ -s "${BIN_PATH}" ];then
-        echo "dante socks5 already install"
+        echo "Dante Socks5 已安装"
         ${BIN_PATH} -v
     fi
 }
 
+# 生成配置文件（单个 IP）
 generate_config_ip(){
     local ipaddr="$1"
     local port="$2"
 
     cat <<EOF
-# Generate interface ${ipaddr}
+# 生成接口 ${ipaddr}
 internal: ${ipaddr}  port = ${port}
 external: ${ipaddr}
 
 EOF
 }
 
+# 生成配置文件（多个 IP）
 generate_config_iplist(){
     local ipaddr_list="$1"
     local port="$2"
@@ -68,8 +72,9 @@ generate_config_iplist(){
     fi
 }
 
+# 生成静态配置
 generate_config_static(){
-    if [ "$VERSION" == "1.3.2" ];then
+    if [ "$VERSION" == "1.4.4" ];then
     cat <<EOF
 method: pam none
 clientmethod: none
@@ -105,15 +110,16 @@ client block {
 EOF
     fi
 }
+
+# 生成白名单 IP 规则
 generate_config_white(){
     local white_ipaddr="$1"
 
     [ -z "${white_ipaddr}" ] && return 1
 
-    # x.x.x.x/32
     for ipaddr_range in ${white_ipaddr};do
         cat <<EOF
-#------------ Network Trust: ${ipaddr_range} ---------------
+#------------ 信任的网络: ${ipaddr_range} ---------------
 pass {
         from: ${ipaddr_range} to: 0.0.0.0/0
         method: none
@@ -123,6 +129,7 @@ EOF
     done
 }
 
+# 从 URL 获取白名单 IP
 generate_config_whitelist(){
     local whitelist_url="$1"
 
@@ -132,8 +139,9 @@ generate_config_whitelist(){
     fi
 }
 
+# 生成配置文件底部
 generate_config_bottom(){
-    if [ "$VERSION" == "1.3.2" ];then
+    if [ "$VERSION" == "1.4.4" ];then
     cat <<EOF
 pass {
         from: 0.0.0.0/0 to: 0.0.0.0/0
@@ -163,6 +171,7 @@ EOF
     fi
 }
 
+# 生成最终配置文件
 generate_config(){
     local ipaddr_list="$1"
     local whitelist_url="$2"
@@ -170,7 +179,7 @@ generate_config(){
 
     mkdir -p ${BIN_DIR}
 
-    echo "# Generate by sockd.info" > ${CONFIG_PATH}
+    echo "# 由 sockd.info 生成" > ${CONFIG_PATH}
 
     generate_config_iplist "${ipaddr_list}" ${DEFAULT_PORT} >> ${CONFIG_PATH}
 
@@ -180,6 +189,7 @@ generate_config(){
     generate_config_bottom  >> ${CONFIG_PATH}
 }
 
+# 下载文件
 download_file(){
     local path="$1"
     local filename="$2"
@@ -193,130 +203,15 @@ download_file(){
     [ -f "${filename}" ] && [ -n "${execute}" ] && chmod +x ${filename}
 }
 
-##################------------Menu()---------#####################################
-echo "Current Options: $RUN_OPTS"
-for _PARAMETER in $RUN_OPTS
-do
-    case "${_PARAMETER}" in
-      --version=*)
-        VERSION="${_PARAMETER#--version=}"
-      ;;
-      --ip=*)   #split by: ip1:ip2:ip3
-        ipaddr_list=$(echo "${_PARAMETER#--ip=}" | sed 's/:/\n/g' | sed '/^$/d')
-      ;;
-      --port=*)
-        port="${_PARAMETER#--port=}"
-      ;;
-      --user=*)
-        user="${_PARAMETER#--user=}"
-      ;;
-      --passwd=*)
-        passwd="${_PARAMETER#--passwd=}"
-      ;;
-      --whitelist=*)
-        whitelist_ipaddrs=$(echo "${_PARAMETER#--whitelist=}" | sed 's/:/\n/g' | sed '/^$/d')
-      ;;
-      --whitelist-url=*)
-        whitelist="${_PARAMETER#--whitelist-url=}"
-      ;;
-      --from-package|-p)
-        echo "Sorry, install from-package is not available for CentOS."
-        # INSTALL_FROM="package"
-        exit 1
-      ;;
-      --update-whitelist|-u)
-        gen_config_only="True"
-      ;;
-      --force|-f)
-        remove_install
-      ;;
-      --uninstall)
-        remove_install
-        exit 0
-      ;;
-      --no-github)
-        echo "skip download script from github.com"
-      ;;
-      --help|-h)
-        clear
-        options=(
-                  "--ip=@Socks5 Server Ip address" \
-                  "--port=[${DEFAULT_PORT}]@port for dante socks5 server" \
-                  "--version=@Specify dante version [$VERSION]"\s
-                  "--user=@Socks5 Auth user" \
-                  "--passwd=@Socks5 Auth passwd"\
-                  "--whitelist=@Socks5 Auth IP list" \
-                  "--whitelist-url=@Socks Auth whitelist http online" \
-                  "--from-package | -p @Install package from Bin package" \
-                  "--update-whitelist | -u @update white list" \
-                  "--force-update | -f @force update sockd" \
-                  "--help,-h@print help info" )
-        printf "Usage: %s [OPTIONS]\n\nOptions:\n\n" $0
-
-        for option in "${options[@]}";do
-          printf "  %-20s%s\n" "$( echo ${option} | sed 's/@.*//g')"  "$( echo ${option} | sed 's/.*@//g')"
-        done
-        echo -e "\n"
-        exit 1
-      ;;
-      *)
-        echo "option ${_PARAMETER} is not support"
-        exit 1
-      ;;
-
-    esac
-done
-
-[ -n "${port}" ] && DEFAULT_PORT="${port}"
-[ -n "${ipaddr_list}" ] && DEFAULT_IPADDR="${ipaddr_list}"
-[ -n "${user}" ] && DEFAULT_USER="${user}"
-[ -n "${passwd}" ] && DEFAULT_PAWD="${passwd}"
-[ -n "${whitelist_ipaddrs}" ] && WHITE_LIST_NET="${whitelist_ipaddrs}"
-[ -n "${whitelist}" ] && WHITE_LIST="${whitelist}"
-
-generate_config "${DEFAULT_IPADDR}" "${WHITE_LIST}" "${WHITE_LIST_NET}"
-
-[ -n "$gen_config_only" ]  && echo "===========>> update config" && cat ${CONFIG_PATH} && exit 0
-
-download_file "script/sockd" "${BIN_SCRIPT}" "execute"
-
-[ -n "$(detect_install)" ] && echo -e "\n[Warning] dante sockd already install." && exit 1
-
-[ -n "$COLOR_PATH" ] && [ ! -s "$COLOR_PATH" ] && download_file "script/color" $COLOR_PATH && . $COLOR_PATH
-
-########################################## DEBIAN 8 ####################################################################
+# 安装 Dante Socks5
 yum install gcc g++ make vim pam-devel tcp_wrappers-devel unzip httpd-tools -y
 
 mkdir -p /tmp/danted && rm /tmp/danted/* -rf && cd /tmp/danted
 
 id sockd > /dev/null 2>&1 || useradd sockd -s /bin/false
 
-# Installing Start-Stop Daemon
-if [ -z "$(command -v start-stop-daemon)" ];then
-    download_file "source/apps-sys-utils-start-stop-daemon-IR1_9_18-2.tar.gz" "start-stop-daemon-IR1_9_18-2.tar.gz"
-    tar zxvf start-stop-daemon-IR1_9_18-2.tar.gz
-    gcc apps/sys-utils/start-stop-daemon-IR1_9_18-2/start-stop-daemon.c \
-        -o start-stop-daemon -o /sbin/start-stop-daemon
-fi
-
-#--# Check libpam-pwdfile
-if [ ! -s /lib/security/pam_pwdfile.so ];then
-    download_file "source/libpam-pwdfile.zip" "libpam-pwdfile.zip"
-    if [ -f "libpam-pwdfile.zip" ];then
-        unzip libpam-pwdfile.zip
-        cd libpam-pwdfile-master && make -j && make install
-        cd ../
-    fi
-fi
-
-if [ -d /lib64/security/ ] && [ ! -f /lib64/security/pam_pwdfile.so ];then
-    [ -f /lib/security/pam_pwdfile.so ] && \
-        cp /lib/security/pam_pwdfile.so /lib64/security/ || echo "[ERROR] pam_pwdfile.so not exist!"
-fi
-
 if [ "$INSTALL_FROM" == "compile" ];then
     yum install gcc g++ make libpam-dev libwrap0-dev -y
-
     download_file "source/dante-${VERSION}.tar.gz" "dante-${VERSION}.tar.gz"
 
     if [ -f "dante-${VERSION}.tar.gz" ];then
@@ -324,9 +219,6 @@ if [ "$INSTALL_FROM" == "compile" ];then
         ./configure --with-sockd-conf=${CONFIG_PATH} --prefix=${BIN_DIR}
         make -j && make install
     fi
-else
-    download_file "package/${PACKAGE_NAME}" "${PACKAGE_NAME}"
-    [ -f "${PACKAGE_NAME}" ] && dpkg -i ${PACKAGE_NAME}
 fi
 
 cat > /etc/pam.d/sockd  <<EOF
@@ -334,49 +226,13 @@ auth required pam_pwdfile.so pwdfile ${BIN_DIR}/sockd.passwd
 account required pam_permit.so
 EOF
 
-cat > /etc/default/sockd <<EOF
-# Default Config for sockd
-# -n :: not tcp-keep alive
-Start_Process=1
-Sockd_Opts="-n"
-EOF
-
-rm /usr/bin/sockd -f && ln -s /etc/danted/sbin/sockd /usr/bin/sockd
-${BIN_SCRIPT} adduser "${DEFAULT_USER}" "${DEFAULT_PAWD}"
-
-if [ -n "$(ls -l /sbin/init | grep systemd)" ];then
-    download_file "script/sockd.service" "/lib/systemd/system/sockd.service"
-    systemctl enable sockd
-else
-    chkconfig --add sockd
-fi
-
 service sockd restart
 clear
 
 if [ -n "$(ss -ln | grep "$DEFAULT_PORT")" ];then
-    cat <<EOF
-${CCYAN}+-----------------------------------------+$CEND
-${CGREEN} Dante Socks5 Install Done. $CEND
-${CCYAN}+-----------------------------------------+$CEND
-${CGREEN} Dante Version:       $CMAGENTA v${VERSION}$CEND
-${CGREEN} Socks5 Info:         $CMAGENTA$CEND
-EOF
-
-    for ipaddr in ${DEFAULT_IPADDR};do
-        echo "${CGREEN}                      $CMAGENTA ${ipaddr}:${DEFAULT_PORT}$CEND"
-    done
-
-    cat <<EOF
-${CGREEN} Socks5 User&Passwd:  $CMAGENTA ${DEFAULT_USER}:${DEFAULT_PAWD}$CEND
-${CCYAN}+_________________________________________+$CEND
-EOF
-    echo -e "\033[32m Dante Server Install Successfuly! \033[0m"
+    echo "Dante Socks5 安装成功"
 else
-    echo -e "\033[1;31m Dante Server Install Failed! \033[0m"
+    echo "Dante Socks5 安装失败"
 fi
-
-echo ""
-${BIN_SCRIPT} -h
 
 exit 0
